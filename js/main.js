41a0021c5a79272253360581267deb878c519aeb
mstvnetwork <mstvindianetworkusa@gmail.com>
2025-05-14 00:27:45 +1000

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Function to navigate to channel category pages
+function navigateTo(category) {
+  // You can either open a new page or load content dynamically
+  // For now, redirecting to a separate HTML file
+  window.location.href = `pages/${category}.html`;
+}
+
+// You can later enhance this to dynamically load logos and videos using AJAX or fetch API
