77f69df3b9552ac6959fb4da9418b977d836904b
mstvnetwork <mstvindianetworkusa@gmail.com>
2025-06-23 14:21:39 +1000

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -1,40 +1,13 @@
-// playlist.js
-
 const playlist = [
-  {
-    title: "Morning Chill Out",
-    start: "10:55",
-    duration: 120,
-    url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
-  },
-  {
-    title: "Mid-Morning Motivation (Gangnam Style)",
-    start: "12:55",
-    duration: 8,
-    url: "https://moiptvhls-i.akamaihd.net/hls/live/652960/bpk-tv/master.m3u8"
-  },
-  {
-    title: "News Update (Tears of Steel Demo)",
-    start: "13:03",
-    duration: 12,
-    url: "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8"
-  },
-  {
-    title: "Afternoon Rock (All Star)",
-    start: "13:15",
-    duration: 10,
-    url: "https://moiptvhls-i.akamaihd.net/hls/live/652960/bpk-tv/master.m3u8"
-  },
-  {
-    title: "Evening Unwind (Despacito)",
-    start: "13:25",
-    duration: 15,
-    url: "https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8"
-  },
-  {
-    title: "Late Night Classic (Me at the zoo)",
-    start: "13:40",
-    duration: 10,
-    url: "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8"
-  }
+  { title: "Morning Chill Out", start: "10:55", duration: 120, url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8" },
+  { title: "Mid-Morning Motivation (Gangnam Style)", start: "12:55", duration: 8, url: "https://moiptvhls-i.akamaihd.net/hls/live/652960/bpk-tv/master.m3u8" },
+  { title: "News Update (Tears of Steel Demo)", start: "13:03", duration: 12, url: "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8" },
+  { title: "Afternoon Rock (All Star)", start: "13:15", duration: 10, url: "https://moiptvhls-i.akamaihd.net/hls/live/652960/bpk-tv/master.m3u8" },
+  { title: "Evening Unwind (Despacito)", start: "13:25", duration: 15, url: "https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8" },
+  { title: "Late Night Classic (Me at the zoo)", start: "13:40", duration: 10, url: "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8" },
+
+  // 🆕 Add these:
+  { title: "Afternoon Encore", start: "13:50", duration: 20, url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8" },
+  { title: "Prime Beats", start: "14:10", duration: 20, url: "https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8" },
+  { title: "Tea Time Chill", start: "14:30", duration: 20, url: "https://moiptvhls-i.akamaihd.net/hls/live/652960/bpk-tv/master.m3u8" }
 ];
