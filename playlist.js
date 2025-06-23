207617af229dabcdbcb78c98b0fcc4eb970dd6f1
mstvnetwork <mstvindianetworkusa@gmail.com>
2025-06-23 14:16:43 +1000

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -1,27 +1,40 @@
 // playlist.js
+
 const playlist = [
   {
-    title: "Segment A",
-    url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
-    start: "01:50",
-    end: "01:52"
+    title: "Morning Chill Out",
+    start: "10:55",
+    duration: 120,
+    url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
   },
   {
-    title: "Segment B",
-    url: "https://moiptvhls-i.akamaihd.net/hls/live/652960/bpk-tv/master.m3u8",
-    start: "01:52",
-    end: "01:54"
+    title: "Mid-Morning Motivation (Gangnam Style)",
+    start: "12:55",
+    duration: 8,
+    url: "https://moiptvhls-i.akamaihd.net/hls/live/652960/bpk-tv/master.m3u8"
   },
   {
-    title: "Segment C",
-    url: "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8",
-    start: "01:54",
-    end: "01:56"
+    title: "News Update (Tears of Steel Demo)",
+    start: "13:03",
+    duration: 12,
+    url: "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8"
   },
   {
-    title: "Segment D",
-    url: "https://moiptvhls-i.akamaihd.net/hls/live/652960/bpk-tv/master.m3u8",
-    start: "01:56",
-    end: "01:58"
+    title: "Afternoon Rock (All Star)",
+    start: "13:15",
+    duration: 10,
+    url: "https://moiptvhls-i.akamaihd.net/hls/live/652960/bpk-tv/master.m3u8"
+  },
+  {
+    title: "Evening Unwind (Despacito)",
+    start: "13:25",
+    duration: 15,
+    url: "https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8"
+  },
+  {
+    title: "Late Night Classic (Me at the zoo)",
+    start: "13:40",
+    duration: 10,
+    url: "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8"
   }
 ];
