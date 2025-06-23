b3c9176fcea8efa01a683a43ed8f0c80571ccb7d
mstvnetwork <mstvindianetworkusa@gmail.com>
2025-06-23 13:59:37 +1000

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -1,16 +1,27 @@
-const schedule = [
+// playlist.js
+const playlist = [
   {
-    title: "Late Night YouTube Show",
-    start: "2025-06-23T00:00:00+10:00", // 12:00 AM
-    duration: 600, // 10 minutes
-    type: "youtube",
-    videoId: "dQw4w9WgXcQ" // Sample video – replace with your own
+    title: "Segment A",
+    url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
+    start: "01:50",
+    end: "01:52"
   },
   {
-    title: "Test HLS Stream",
-    start: "2025-06-23T00:10:00+10:00", // Starts after first one ends
-    duration: 600, // 10 minutes
-    type: "m3u8",
-    url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+    title: "Segment B",
+    url: "https://moiptvhls-i.akamaihd.net/hls/live/652960/bpk-tv/master.m3u8",
+    start: "01:52",
+    end: "01:54"
+  },
+  {
+    title: "Segment C",
+    url: "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8",
+    start: "01:54",
+    end: "01:56"
+  },
+  {
+    title: "Segment D",
+    url: "https://moiptvhls-i.akamaihd.net/hls/live/652960/bpk-tv/master.m3u8",
+    start: "01:56",
+    end: "01:58"
   }
 ];
