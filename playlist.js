140517bfe0234d22fd10460651c384468f93e166
mstvnetwork <mstvindianetworkusa@gmail.com>
2025-06-23 14:40:07 +1000

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -1,11 +1,14 @@
 const playlist = [
-  { title: "Morning Chill Out", start: "10:55", duration: 120, url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8" },
-  { title: "Mid-Morning Motivation (Gangnam Style)", start: "12:55", duration: 8, url: "https://moiptvhls-i.akamaihd.net/hls/live/652960/bpk-tv/master.m3u8" },
-  { title: "News Update (Tears of Steel Demo)", start: "13:03", duration: 12, url: "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8" },
-  { title: "Afternoon Rock (All Star)", start: "13:15", duration: 10, url: "https://moiptvhls-i.akamaihd.net/hls/live/652960/bpk-tv/master.m3u8" },
-  { title: "Evening Unwind (Despacito)", start: "13:25", duration: 15, url: "https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8" },
-  { title: "Late Night Classic (Me at the zoo)", start: "13:40", duration: 10, url: "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8" },
-
-  // 🔥 NEW slot to make it play now!
-  { title: "Afternoon Encore", start: "14:25", duration: 15, url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8" }
+  {
+    title: "Now Playing: Time Match Test",
+    start: "14:30",
+    duration: 15, // ends at 14:45
+    url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+  },
+  {
+    title: "Next Segment: After 14:45",
+    start: "14:45",
+    duration: 15, // ends at 15:00
+    url: "https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8"
+  }
 ];
