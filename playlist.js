e181d1097005125afa7a83fc33c98e1dd78d79a3
mstvnetwork <mstvindianetworkusa@gmail.com>
2025-06-23 14:29:47 +1000

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -6,8 +6,6 @@ const playlist = [
   { title: "Evening Unwind (Despacito)", start: "13:25", duration: 15, url: "https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8" },
   { title: "Late Night Classic (Me at the zoo)", start: "13:40", duration: 10, url: "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8" },
 
-  // 🆕 Add these:
-  { title: "Afternoon Encore", start: "13:50", duration: 20, url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8" },
-  { title: "Prime Beats", start: "14:10", duration: 20, url: "https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8" },
-  { title: "Tea Time Chill", start: "14:30", duration: 20, url: "https://moiptvhls-i.akamaihd.net/hls/live/652960/bpk-tv/master.m3u8" }
+  // 🔥 NEW slot to make it play now!
+  { title: "Afternoon Encore", start: "14:25", duration: 15, url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8" }
 ];
